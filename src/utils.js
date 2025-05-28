722cb3ce08d4a4866ec1a619a41e5797e902a711
KakraGeek <desmond.asiedu@gmail.com>
2025-05-28 09:24:15 +0000

    Update utilities and other files

--- src/utils.js
+++ src/utils.js
@@ -26,3 +26,59 @@ export async function getIdeaById(id) {
 }
 
 export { dataDir, ideasPath }; 
+
+const fs = require('fs-extra');
+const path = require('path');
+const { handleError } = require('./errorHandler');
+
+const dataDir = path.join(process.cwd(), 'data');
+const ideasPath = path.join(dataDir, 'ideas.json');
+
+async function getIdeas() {
+  try {
+    await fs.ensureDir(dataDir);
+    await fs.ensureFile(ideasPath);
+    
+    try {
+      const data = await fs.readJson(ideasPath);
+      return Array.isArray(data) ? data : [];
+    } catch (error) {
+      if (error.name === 'SyntaxError') {
+        handleError(error, 'json');
+        return [];
+      }
+      throw error;
+    }
+  } catch (error) {
+    handleError(error, 'reading ideas');
+    return [];
+  }
+}
+
+async function saveIdeas(ideas) {
+  try {
+    await fs.ensureDir(dataDir);
+    await fs.writeJson(ideasPath, ideas, { spaces: 2 });
+  } catch (error) {
+    handleError(error, 'saving ideas');
+    throw error;
+  }
+}
+
+async function getIdeaById(id) {
+  try {
+    const ideas = await getIdeas();
+    return ideas.find(idea => idea.id === id);
+  } catch (error) {
+    handleError(error, 'finding idea');
+    return null;
+  }
+}
+
+module.exports = {
+  getIdeas,
+  saveIdeas,
+  getIdeaById,
+  dataDir,
+  ideasPath
+};
