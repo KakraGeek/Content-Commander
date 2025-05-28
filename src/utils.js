7b63b0b7b86282e181008e21d062ddcfe79a21c2
KakraGeek <desmond.asiedu@gmail.com>
2025-05-28 10:13:55 +0000

    Add configuration system

--- src/utils.js
+++ src/utils.js
@@ -1,44 +1,14 @@
 import fs from 'fs-extra';
 import path from 'path';
+import { handleError } from './errorHandler.js';
 
 const dataDir = path.join(process.cwd(), 'data');
 const ideasPath = path.join(dataDir, 'ideas.json');
 
 export async function getIdeas() {
-  await fs.ensureDir(dataDir);
-  await fs.ensureFile(ideasPath);
-  try {
-    const data = await fs.readJson(ideasPath);
-    return Array.isArray(data) ? data : [];
-  } catch (error) {
-    return [];
-  }
-}
-
-export async function saveIdeas(ideas) {
-  await fs.ensureDir(dataDir);
-  await fs.writeJson(ideasPath, ideas, { spaces: 2 });
-}
-
-export async function getIdeaById(id) {
-  const ideas = await getIdeas();
-  return ideas.find(idea => idea.id === id);
-}
-
-export { dataDir, ideasPath }; 
-
-const fs = require('fs-extra');
-const path = require('path');
-const { handleError } = require('./errorHandler');
-
-const dataDir = path.join(process.cwd(), 'data');
-const ideasPath = path.join(dataDir, 'ideas.json');
-
-async function getIdeas() {
   try {
     await fs.ensureDir(dataDir);
     await fs.ensureFile(ideasPath);
-    
     try {
       const data = await fs.readJson(ideasPath);
       return Array.isArray(data) ? data : [];
@@ -55,7 +25,7 @@ async function getIdeas() {
   }
 }
 
-async function saveIdeas(ideas) {
+export async function saveIdeas(ideas) {
   try {
     await fs.ensureDir(dataDir);
     await fs.writeJson(ideasPath, ideas, { spaces: 2 });
@@ -65,7 +35,7 @@ async function saveIdeas(ideas) {
   }
 }
 
-async function getIdeaById(id) {
+export async function getIdeaById(id) {
   try {
     const ideas = await getIdeas();
     return ideas.find(idea => idea.id === id);
@@ -75,10 +45,4 @@ async function getIdeaById(id) {
   }
 }
 
-module.exports = {
-  getIdeas,
-  saveIdeas,
-  getIdeaById,
-  dataDir,
-  ideasPath
-};
+export { dataDir, ideasPath };
